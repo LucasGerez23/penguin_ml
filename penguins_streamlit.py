b74638462777be3da369b4755e04d9e76e12ed28
Lucas Gerez <lucas@geotec.io>
2025-05-27 19:14:11 -0300

    Agrego password con Streamlit secrets

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -15,6 +15,11 @@ st.write("""This app uses 6 inputs to predict the species of penguin using
     a model built on the Palmer Penguins dataset. Use the form below
     to get started!"""
 )
+
+password_guess = st.text_input('What is the Password?')
+if password_guess != st.secrets['password']:
+ st.stop()
+
 try:
    penguin_file = st.file_uploader('Upload your own penguin data')
 
